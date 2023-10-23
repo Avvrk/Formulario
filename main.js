f79b5209c23a2d0a27a40ab9647d401ca3c54fa0
Avvrk <jomaldonado480@gmail.com>
2023-10-23 18:06:24 -0500

    Se actualizaron los estilos del formulario para mejorar su apariencia. Además, se agregó una tabla para mostrar los registros de usuarios. También se cambió el tipo de dato del campo 'género' a select.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const arr = []
+let datos = [];
+let operacion = null;
+let indice = null;
 
 function registro() {
     
@@ -6,12 +8,11 @@ function registro() {
     let apellido = document.getElementById("apellido").value;
     let tipoDocumento = document.getElementById("fm").value;
     let numDocumento = document.getElementById("numDocumento").value;
-    let genero = document.querySelector('input[name="genero"]:checked').value;
+    let genero = document.getElementById("genr").value;
     let telefono = document.getElementById("telefono").value;
     let correo = document.getElementById("correo").value;
     let fecNacimiento = document.getElementById("fecNacimiento").value;
 
-    
     let datosUsuario = {
         nombre: nombre,
         apellido: apellido,
@@ -23,20 +24,39 @@ function registro() {
         fecNacimiento: fecNacimiento
     };
 
-    arr.push(datosUsuario)
-   
+    if (operacion === true) {
+        datos[indice].nombre = document.getElementById("nombre").value;
+        datos[indice].apellido = document.getElementById("apellido").value;
+        datos[indice].tipoDocumento = document.getElementById("fm").value;
+        datos[indice].numDocumento = document.getElementById("numDocumento").value;
+        datos[indice].genero = document.getElementById("genr").value;
+        datos[indice].telefono = document.getElementById("telefono").value;
+        datos[indice].correo = document.getElementById("correo").value;
+        datos[indice].fecNacimiento = document.getElementById("fecNacimiento").value;
+    } else {
+        datos.push(datosUsuario);
+    }
+    
+    document.querySelector("#ac").value = "Aceptar";
+
     console.log(datosUsuario);
 
     swal("¡Registro Exitoso!", "Tu registro se ha completado con éxito.", "success");
+
+    operacion = false;
+
+    document.getElementById("tablaa").innerHTML = "";
+
+    pintar();
 }
 
 function validacion() {
 
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
-    let tipoDocumento = document.getElementById("fm").value;
+    // let tipoDocumento = document.getElementById("fm").value;
     let numDocumento = document.getElementById("numDocumento").value;
-    let genero = document.querySelector('input[name="genero"]:checked');
+    // let genero = document.getElementById("genr").value;
     let telefono = document.getElementById("telefono").value;
     let correo = document.getElementById("correo").value;
     let fecNacimiento = document.getElementById("fecNacimiento").value;
@@ -45,6 +65,7 @@ function validacion() {
     const hoy = new Date();
     const edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
 
+    
     if (nombre == "")
         swal("Error", "Por favor, llena el campo de nombre.", "error");
     else if (apellido == "")
@@ -55,8 +76,6 @@ function validacion() {
         swal("Error", "Por favor, ingresa un número de documento válido (solo números).", "error");
     else if (numDocumento.length <= 7 || numDocumento.length >= 11)
         swal("Error", "El número de documento debe tener entre 8 y 10 dígitos.", "error");
-    else if (genero == null)
-        swal("Error", "Por favor, selecciona un género.", "error");
     else if (telefono == "")
         swal("Error", "Por favor, ingresa tu número de teléfono.", "error");
     else if (isNaN(telefono))
@@ -71,7 +90,84 @@ function validacion() {
         swal("Error", "Por favor, ingresa tu fecha de nacimiento.", "error");
     else if (edad < 18)
         swal("Error", "No puedes registrarte en este momento, debes tener autorización de tus padres.", "error");
-    else 
-       registro()
+    else {
+        registro();
+    }
+    
 }
 
+function pintar() {
+    let frag = document.createDocumentFragment()
+
+    datos.forEach((item, index) => {
+        let tr = document.createElement("tr");
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        let td3 = document.createElement("td");
+        let td4 = document.createElement("td");
+        let td5 = document.createElement("td");
+        let td6 = document.createElement("td");
+        let td7 = document.createElement("td");
+        let td8 = document.createElement("td");
+        let td9 = document.createElement("td");
+        let editar = document.createElement("button");
+        let eliminar = document.createElement("button");
+
+        editar.textContent = "📝";
+        editar.addEventListener("click", () => {
+            document.querySelector("#ac").value = "Actualizar";
+            edit(item, index);
+        });
+        eliminar.textContent = "❌";
+        eliminar.addEventListener("click", () => {
+            delet(index);
+        })
+        td1.textContent = item.nombre;
+        td2.textContent = item.apellido;
+        td3.textContent = item.tipoDocumento;
+        td4.textContent = item.numDocumento;
+        td5.textContent = item.genero;
+        td6.textContent = item.telefono;
+        td7.textContent = item.correo;
+        td8.textContent = item.fecNacimiento;
+
+        td9.appendChild(editar);
+        td9.appendChild(eliminar);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        tr.appendChild(td6);
+        tr.appendChild(td7);
+        tr.appendChild(td8);
+        tr.appendChild(td9);
+        frag.appendChild(tr);
+
+        document.getElementById("tablaa").appendChild(frag);
+    })
+
+    function edit(item,i){
+        operacion = true;
+        indice = i;
+        console.log(item);
+        document.getElementById("nombre").value = item.nombre;
+        document.getElementById("apellido").value = item.apellido;
+        document.getElementById("fm").value = item.tipoDocumento;
+        document.getElementById("numDocumento").value = item.numDocumento;
+        document.getElementById("genr").value = item.genero;
+        document.getElementById("telefono").value = item.telefono;
+        document.getElementById("correo").value = item.correo;
+        document.getElementById("fecNacimiento").value = item.fecNacimiento;
+    }
+
+    function delet(i) {
+        indice = i;
+        datos.splice(indice, 1);
+
+        document.getElementById("tablaa").innerHTML = "";
+        pintar();
+    }
+
+
+}
